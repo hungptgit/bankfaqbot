94027697acc7f629ede1c87ecf1f539e364e1e9c
hungptgit <phantranhung@gmail.com>
2016-06-20 15:04:14 +0700

    Update server.js

--- server.js
+++ server.js
@@ -197,8 +197,8 @@ function formatmsg(msg){
     return msg.substr(0,msg.lastIndexOf(".")+1);
 }
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.IP || "127.0.0.1");
+app.set('port', process.env.PORT || 3002);
+app.set('ip', process.env.IP || "127.0.0.1");
 
 server.listen(app.get('port'), app.get('ip'), function() {
   console.log("Chat bot server listening at %s:%d ", app.get('ip'), app.get('port'));
