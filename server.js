beb695fef2f5a12fb50ba00966e476e1435f6294
phantranhung <phantranhung@gmail.com>
2017-09-04 22:13:16 -0400

    nghi xong

--- server.js
+++ server.js
@@ -59,16 +59,16 @@ agenda.on('ready', () => {
 					case 'GET_STARTED_PAYLOAD':
 						f.txt(sender, 'Xin chao ban! Chuc ban mot ngay tot lanh. Hay lua chon cac tinh nang');
 						break;
-					case 'PB_INQ_BALANCE_PAYLOAD':
+					case 'menu:INQ_BALANCE_PAYLOAD':
 						f.txt(sender, 'Ban muon van tin so du tai khoan: Tien gui? Tien vay ?');
 						break;	
-					case 'PB_XFER_PAYLOAD':
+					case 'menu:XFER_PAYLOAD':
 						f.txt(sender, 'Ban muon chuyen khoan trong hay ngoai he thong');
 						break;
-					case 'PB_PAYMENT_PAYLOAD':
+					case 'menu:PAYMENT_PAYLOAD':
 						f.txt(sender, 'Ban muon thanh toan cho');
 						break;
-					case 'PB_SAVING_PAYLOAD':
+					case 'menu:SAVING_PAYLOAD':
 						f.txt(sender, 'Ban tham khao bieu lai suat gui tiet kiem cho cac ky han tai website VietinBank. Ban muon gui tiet kiem ky han nao? 1 thang - 2 thang - 3 thang - 6 thang - 9 thang - 12 thang');
 						break;
 					default:
@@ -137,49 +137,6 @@ agenda.on('ready', () => {
 	agenda.start();
 });
 
-/*
-json:{
-"persistent_menu":[
-    {
-      "locale":"default",
-      "composer_input_disabled":true,
-      "call_to_actions":[
-        {
-          "title":"My Account",
-          "type":"nested",
-          "call_to_actions":[
-            {
-              "title":"Pay Bill",
-              "type":"postback",
-              "payload":"PAYBILL_PAYLOAD"
-            },
-            {
-              "title":"History",
-              "type":"postback",
-              "payload":"HISTORY_PAYLOAD"
-            },
-            {
-              "title":"Contact Info",
-              "type":"postback",
-              "payload":"CONTACT_INFO_PAYLOAD"
-            }
-          ]
-        },
-        {
-          "type":"web_url",
-          "title":"Latest News",
-          "url":"http://foxnews.com",
-          "webview_height_ratio":"full"
-        }
-      ]
-    },
-    {
-      "locale":"zh_CN",
-      "composer_input_disabled":false
-    }
-    ]
-    }
-*/
 // Persistent Menu
 f.showPersistent(
 	{"persistent_menu":
@@ -190,36 +147,26 @@ f.showPersistent(
 			{
 				"type": "postback",
 				"title": "Xem số dư",
-				"payload": "PB_INQ_BALANCE_PAYLOAD"
+				"payload": "menu:INQ_BALANCE_PAYLOAD"
 			},
 			{
 				"type": "postback",
 				"title": "Chuyển khoản",
-				"payload": "PB_XFER_PAYLOAD"
+				"payload": "menu:XFER_PAYLOAD"
 			},								
-			{
-				"type": "postback",
-				"title": "Thanh toán hoá đơn",
-				"payload": "PB_PAYMENT_PAYLOAD"
-			},
-			{
-				"type": "postback",
-				"title": "Gửi tiết kiệm",
-				"payload": "PB_SAVING_PAYLOAD"
-			},
 			{
 				"title":"Dịch vụ khác",
         "type":"nested",
         "call_to_actions":[
             {
-              "title":"Tính lãi vay",
+              "title":"Thanh toán",
               "type":"postback",
-              "payload":"PB_CALC_LOAN_PAYLOAD"
+              "payload":"menu:PAYMENT_PAYLOAD"
             },
             {
-              "title":"Lấy xem mã dự thươngr",
+              "title":"Gửi tiết kiệm",
               "type":"postback",
-              "payload":"PB_PROMO_PAYLOAD"
+              "payload":"menu:SAVING_PAYLOAD"
             },
 						{
 							"type": "web_url",
@@ -227,17 +174,7 @@ f.showPersistent(
 							"payload": "http://vietinbank.vn/"
 						}
           ]
-			},	
-			{
-				"type": "web_url",
-				"title": "Tỷ giá, lãi xuất",
-				"url": "http://vietinbank.vn/"
-			},	
-			{
-				"type": "web_url",
-				"title": "Khảo sát nhanh",
-				"url": "http://vietinbank.vn/"
-			}	
+			}
 		]},
     {
       "locale":"vi_VN",
@@ -248,4 +185,4 @@ f.showPersistent(
 // Subscribe
 f.subscribe();
 
-server.listen(PORT, () => console.log(`Chi running on port ${PORT}`));
+server.listen(PORT, () => console.log(`VietinBanker running on port ${PORT}`));
