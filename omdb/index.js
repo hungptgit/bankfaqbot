edf8584ff3edc7036ede95033c3f5ba7cceb6c64
phantranhung <phantranhung@gmail.com>
2017-09-06 04:20:51 -0400

    review 1

--- omdb/index.js
+++ omdb/index.js
@@ -3,11 +3,14 @@ const request = require('request');
 const createResponse = require('../utils');
 const config = require('../config');
 const getInfo = data => {
-  let intent = data.entities.intent && data.entities.intent[0].value || 'movieInfo';
+  console.log('data: ' + JSON.stringify(data));
+  let intent = data.entities.intent && data.entities.intent[0].value || 'undefined';
   //let movie = data.entities.movie && data.entities.movie[0].value || null;
   //let releaseYear = data.entities.releaseYear && data.entities.releaseYear[0].value || null;
   return new Promise((resolve, reject) => {
     if(intent) {
+      console.log('intent: ' + intent);
+      
       // Fetch data from DB
       resolve(createResponse(intent, 'Du lieu de tao response'));
     } else {
