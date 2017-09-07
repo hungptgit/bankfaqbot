f78260b608d184943f96d8a2f15614c74c045cfe
phantranhung <phantranhung@gmail.com>
2017-09-07 06:51:12 -0400

    vuon gia vi

--- fbeamer/index.js
+++ fbeamer/index.js
@@ -146,7 +146,7 @@ class FBeamer {
 				console.log('showPersistent result:' + JSON.stringify(response.body));
 			}
 			else {
-				console.log(error);
+				console.log('showPersistent error:' + error);
 			}
 		});
 	}
