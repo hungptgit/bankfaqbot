1c1ce555a2a637d8145fd4661581924be3b8e28d
phantranhung <phantranhung@gmail.com>
2017-11-09 00:26:09 -0500

    codeanw

--- model/index.js
+++ model/index.js
@@ -20,6 +20,27 @@ class Model {
       }
     });
   }
+  
+  add(field, content, tag) {
+    var collection = db.collection('questions');
+    //Create some document
+    var questionItem = {
+      field: field,
+      content: content,
+      state: 'MA',
+      tag: tag
+    };
+
+    // Insert some items
+    collection.insert([questionItem], function(err, result) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Inserted %d documents into the "question" collection. The documents inserted with "_id" are:', result.length, result);
+      }
+    });
+
+  }
 }
 
 module.exports = Model;
