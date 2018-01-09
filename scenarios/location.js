b006406e4e968810718bca58fad582e5d7aa08f5
redhungpt <redhungpt@yahoo.com>
2018-01-09 06:22:32 -0500

    a

--- scenarios/location.js
+++ scenarios/location.js
@@ -51,8 +51,89 @@ class Location {
           displayIndex = locations.length;
         }
 
+        var arrayLocationDisplay = [];
+        /*
+        var arrayLocationDisplay = [{
+            title: "Vietinbank atm",
+            image_url: "http://www.vietnamhotels.biz/littlehanoihostel2/Little-Hanoi-Hostel-2-Google-Map.jpg",
+            subtitle: "87 Trần Hưng Đạo",
+            default_action: {
+              type: "web_url",
+              url: "https://www.google.com/maps/dir/21.0320622,105.8398501/21.0277644,105.8341598",
+              //messenger_extensions: true,
+              //webview_height_ratio: "tall",
+              //fallback_url: "https://ebanking.vietinbank.vn/rcas/portal/web/retail/bflogin"
+            },
+            buttons: [{
+              type: "web_url",
+              url: "https://www.google.com/maps/dir/21.0320622,105.8398501/21.0277644,105.8341598",
+              title: "Chỉ dẫn"
+            }]
+          },
+          {
+            title: "87 Trần Hưng Đạo",
+            image_url: "http://www.worldeasyguides.com/wp-content/uploads/2012/11/Map-of-Hanoi.jpg",
+            subtitle: "87 Trần Hưng Đạo",
+            default_action: {
+              type: "web_url",
+              url: "https://www.google.com/maps/dir/21.0320622,105.8398501/21.0277644,105.8341598",
+              //messenger_extensions: true,
+              //webview_height_ratio: "tall",
+              //fallback_url: "https://ebanking.vietinbank.vn/rcas/portal/web/retail/bflogin"
+            },
+            buttons: [{
+              type: "web_url",
+              url: "https://www.google.com/maps/dir/21.0320622,105.8398501/21.0277644,105.8341598",
+              title: "Chỉ dẫn"
+            }]
+          },
+          {
+            title: "ATM Vietinbank - Haphaco - Hiệu Thuốc 8-3",
+            image_url: "http://www.vietnamhotels.biz/littlehanoihostel2/Little-Hanoi-Hostel-2-Google-Map.jpg",
+            subtitle: "5, Phố Cửa Nam, Phường Cửa Nam, Quận Hoàn Kiếm, Cửa Nam",
+            default_action: {
+              type: "web_url",
+              url: "https://www.google.com/maps/dir/21.0320622,105.8398501/21.0277644,105.8341598",
+              //messenger_extensions: true,
+              //webview_height_ratio: "tall",
+              //fallback_url: "https://peterssendreceiveapp.ngrok.io/"
+            },
+            buttons: [{
+              type: "web_url",
+              url: "https://www.google.com/maps/dir/21.0320622,105.8398501/21.0277644,105.8341598",
+              title: "Chỉ dẫn"
+            }]
+          }
+        ];
+        */
+        
+        for (var i = 0; i < displayIndex; i++) {
+          var displayLoc = locations[i];
+          console.log('getAtmLocation: ' + i + ' >>> ' + JSON.stringify(displayLoc));
+          var targetLoc = displayLoc.geometry.location.lat + ',' + displayLoc.geometry.location.lng;
+          var gmapUrl = "https://www.google.com/maps/dir/" + location + "/" + targetLoc;
+          var imgUrl = "http://www.vietnamhotels.biz/littlehanoihostel2/Little-Hanoi-Hostel-2-Google-Map.jpg";
+          arrayLocationDisplay.push({
+            title: displayLoc.name,
+            image_url: imgUrl,
+            subtitle: displayLoc.vicinity,
+            default_action: {
+              type: "web_url",
+              url: gmapUrl,
+              //messenger_extensions: true,
+              //webview_height_ratio: "tall",
+              //fallback_url: "https://peterssendreceiveapp.ngrok.io/"
+            },
+            buttons: [{
+              type: "web_url",
+              url: gmapUrl,
+              title: "Chỉ dẫn"
+            })
+          });
 
-        let obj = {
+        }
+
+        var obj = {
           recipient: {
             id: sender
           },
@@ -61,53 +142,16 @@ class Location {
               type: "template",
               payload: {
                 template_type: "generic",
-                elements: [{
-                    title: "87 Trần Hưng Đạo",
-                    image_url: "http://www.vietnamhotels.biz/littlehanoihostel2/Little-Hanoi-Hostel-2-Google-Map.jpg",
-                    subtitle: "87 Trần Hưng Đạo",
-                    default_action: {
-                      type: "web_url",
-                      url: "https://www.google.com/maps/dir/21.0320622,105.8398501/21.0277644,105.8341598",
-                      //messenger_extensions: true,
-                      //webview_height_ratio: "tall",
-                      //fallback_url: "https://ebanking.vietinbank.vn/rcas/portal/web/retail/bflogin"
-                    },
-                    buttons: [{
-                      type: "web_url",
-                      url: "https://www.google.com/maps/dir/21.0320622,105.8398501/21.0277644,105.8341598",
-                      title: "Chỉ dẫn"
-                    }]
-                  },
-                  {
-                    title: "ATM Vietinbank - Haphaco - Hiệu Thuốc 8-3",
-                    image_url: "http://www.vietnamhotels.biz/littlehanoihostel2/Little-Hanoi-Hostel-2-Google-Map.jpg",
-                    subtitle: "5, Phố Cửa Nam, Phường Cửa Nam, Quận Hoàn Kiếm, Cửa Nam",
-                    default_action: {
-                      type: "web_url",
-                      url: "https://www.google.com/maps/dir/21.0320622,105.8398501/21.0277644,105.8341598",
-                      //messenger_extensions: true,
-                      //webview_height_ratio: "tall",
-                      //fallback_url: "https://peterssendreceiveapp.ngrok.io/"
-                    },
-                    buttons: [{
-                      type: "web_url",
-                      url: "https://www.google.com/maps/dir/21.0320622,105.8398501/21.0277644,105.8341598",
-                      title: "Chỉ dẫn"
-                    }]
-                  }
-                ]
+                elements: arrayLocationDisplay
               }
             }
           }
         }
-        
+
         f.sendNews(obj)
-      .catch(error => console.log('news: ' + error));
-        
-        for (var i = 0; i < displayIndex; i++) {
-          var randLoc = locations[i];
-          console.log('getAtmLocation: ' + i + ' >>> ' + JSON.stringify(randLoc));
-        }
+          .catch(error => console.log('news: ' + error));
+
+
         return locations;
       });
     }).on('error', function(e) {
