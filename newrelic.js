3dc744e96edc0332f3a857180d042c535965a1e6
redhungpt <redhungpt@yahoo.com>
2018-01-09 22:47:47 -0500

    a

---
+++ newrelic.js
@@ -0,0 +1,25 @@
+'use strict'
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['VietinBankFAQChatBot'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '262abbd6d2a2c398839ec3620c92e01856ccf64c',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
