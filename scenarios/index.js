33e171b153c8dbc7e7b8c0e8bed59e1e40147a6a
redhungpt <redhungpt@yahoo.com>
2018-01-14 23:38:30 -0500

    a

--- scenarios/index.js
+++ scenarios/index.js
@@ -201,13 +201,64 @@ class Scenario {
             news.menu(sender, f);
 
             // sent mail to remind train bot
-            this.sendNotifyMail(senderName, messageTxt, answer, score);
+            // sent mail to remind train bot
+            nodemailer.createTestAccount((err, account) => {
+              // create reusable transporter object using the default SMTP transport
+              let transporter = nodemailer.createTransport({
+                host: config.SMTP_SERVER,
+                port: 465,
+                secure: true, // true for 465, false for other ports
+                requireTLS: true,
+                auth: {
+                  user: config.SMTP_USER, // generated ethereal user
+                  pass: config.SMTP_PASS // generated ethereal password
+                }
+              });
+
+              let mailSubject = 'VietinBank ChatBot: ' + messageTxt;
+
+              let plaintTextContent = senderName + ' said: ' + messageTxt + '\n';
+              plaintTextContent = plaintTextContent + 'Bot reply: ' + answer + ' \n';
+              plaintTextContent = plaintTextContent + 'Score: ' + score + ' \n';
+              plaintTextContent = plaintTextContent + 'Please retrain the bot to make higher score \n';
+
+              let htmlContent = '';
+              htmlContent = htmlContent + '<table rules="all" style="border-color: #666;" cellpadding="10">';
+              htmlContent = htmlContent + '<tr style=\'background: #ffa73c;\'><td> </td><td></td></tr>';
+              htmlContent = htmlContent + '<tr><td><strong>' + senderName + ' said:</strong> </td><td>' + messageTxt + '</td></tr>';
+              htmlContent = htmlContent + '<tr><td><strong>Bot reply:</strong> </td><td>' + answer + '</td></tr>';
+              htmlContent = htmlContent + '<tr><td><strong>Score:</strong> </td><td>' + score + '</td></tr>';
+              htmlContent = htmlContent + '<tr><td><strong>Note:</strong> </td><td>Please retrain the bot to make higher score </td></tr>';
+              htmlContent = htmlContent + '</table>';
+
+              // setup email data with unicode symbols
+              let mailOptions = {
+                from: '"VietinBank FaQ ChatBot" <vietinbankchatbot@gmail.com>', // sender address
+                to: config.QnA_ADMIN_MAIL, // list of receivers
+                subject: mailSubject, // Subject line
+                text: plaintTextContent, // plain text body
+                html: htmlContent // html body
+              };
+
+              console.log('Start sent from: %s', mailOptions.from);
+              // send mail with defined transport object
+              transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                  return console.log(error);
+                }
+                console.log('Message sent: %s', info.messageId);
+                // Preview only available when sending through an Ethereal account
+                console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+              });
+            });
             return;
           }
         }
       });
   }
-
+  
+  /*
   sendNotifyMail(senderName, messageTxt, answer, score) {
     try {
       // sent mail to remind train bot
@@ -265,7 +316,8 @@ class Scenario {
       console.log(JSON.stringify(e));
     }
   }
-
+  */
+  
   processMessage(sender, message, f, wit) {
     return new Promise((resolve, reject) => {
       let buttons = '';
@@ -463,7 +515,7 @@ class Scenario {
       let locLong = coord.long;
       loca.getAtmLocation(sender, locLat, locLong, f);
       //loca.getAtmLocationByText(sender, '108 Tran Hung Dao, Ha Noi', f);
-      
+
     }
   }
 }
