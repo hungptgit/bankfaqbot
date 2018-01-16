b8fa4775a2f2bc3638ef823970120143bf7a85b2
redhungpt <redhungpt@yahoo.com>
2018-01-16 04:59:42 -0500

    a

--- wake_up_dyno.js
+++ wake_up_dyno.js
@@ -3,20 +3,20 @@
 var http = require('http'); //importing http
 
 var options = {
-    host: 'vtbbankbot.herokuapp.com/',
-    port: 80,
-    path: '/WAKEUP_DYNO'
+  host: 'vtbbankbot.herokuapp.com/',
+  port: 80,
+  path: '/'
 };
 console.log("======WAKUP DYNO START");
 http.get(options, function(res) {
-    res.on('data', function(chunk) {
-        try {
-            // optional logging... disable after it's working
-            console.log("======WAKUP DYNO: HEROKU RESPONSE: " + chunk);
-        } catch (err) {
-            console.log(err.message);
-        }
-    });
+  res.on('data', function(chunk) {
+    try {
+      // optional logging... disable after it's working
+      console.log("======WAKUP DYNO: HEROKU RESPONSE: " + chunk);
+    } catch (err) {
+      console.log(err.message);
+    }
+  });
 }).on('error', function(err) {
-    console.log("Error: " + err.message);
+  console.log("Error: " + err.message);
 });
